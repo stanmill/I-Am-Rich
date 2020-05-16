0b96a74fcebde7be843ee3c25423664b741aa933
stanmill <stanley.pena29@gmail.com>
2020-05-15 17:16:02 -0700

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by stanley pena on 5/15/20.
+//  Copyright © 2020 stanley pena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
